ecae3299ec2a6445aab215cef274c9360e33fdb7
Dave Dopson <ddopson@gmail.com>
2013-01-14 14:21:57 -0800

    better debug output in the test runner

--- test/run.js
+++ test/run.js
@@ -10,12 +10,25 @@ var jade = require('../')
 
 // test cases
 
+function three_digits(n) {
+  if (n < 10) return "00" + n;
+  if (n < 100) return "0" + n;
+  return "" + n;
+}
+
+function lineify(str, prefix) {
+  var n = 0;
+  prefix = prefix || '';
+  if (!(typeof str == 'string')) return str
+  return str.replace(/^/mg, function () { return prefix + three_digits(++n) + "  "});
+}
+
 function casesForExt(path, ext) {
   return fs.readdirSync(path).filter(function(file){
     return file.match(ext)
   }).map(function(file){
     return {
-      name: file.replace(ext, '').replace(/[-.]/g, ' '),
+      name: file.replace(ext, ''), // .replace(/[-.]/g, ' '),
       jade_path: path + '/' + file,
       html_path: path + '/' + file.replace(ext, '.html'),
     }
@@ -24,23 +37,25 @@ function casesForExt(path, ext) {
 
 casesForExt('test/cases', /[.]jade$/).forEach(function(test){
   it(test.name, function(){
-    var str = fs.readFileSync(test.jade_path, 'utf8');
-    var html = fs.readFileSync(test.html_path, 'utf8').trim().replace(/\r/g, '');
-    var fn = jade.compile(str, { filename: test.jade_path, pretty: true });
-    var actual = fn({ title: 'Jade' });
-    actual.trim().should.equal(html);
+    try {
+      var str = fs.readFileSync(test.jade_path, 'utf8');
+      var html = fs.readFileSync(test.html_path, 'utf8').trim().replace(/\r/g, '');
+      var fn = jade.compile(str, { filename: test.jade_path, pretty: true });
+      var actual = fn({ title: 'Jade' });
+      actual.trim().should.equal(html);
+    } catch (e) {
+      var ast = jade.parse(str, { filename: test.jade_path, pretty: true});
+      var js = jade.compile(str, { filename: test.jade_path, pretty: true, source: true});
+      //console.log("\nAST:\n" + lineify(JSON.stringify(ast, true, '  '), test.name + '[AST]:'));
+      console.log("\nAST:\n" + lineify(ast.pretty(), test.name + '[AST]:'));
+      console.log("\nJavaScript:\n" + lineify(""+js, test.name + '[JS]:'));
+      throw e
+    }
   })
 });
 
-
-function lineify(str) {
-  var n = 0;
-  if (!(typeof str == 'string')) return str
-  return str.replace(/^/mg, function () { return "" + ++n + "  "});
-}
-
 casesForExt('test/coffee_cases', /[.]jadec$/).forEach(function(test){
-  it(test.name, function(){
+  it("JadeC: " + test.name, function(){
     var str = fs.readFileSync(test.jade_path, 'utf8');
     var html = fs.readFileSync(test.html_path, 'utf8').trim().replace(/\r/g, '');
     var coffee = jade.compile(str, { filename: test.jade_path, pretty: true, coffee: true });
@@ -54,8 +69,10 @@ casesForExt('test/coffee_cases', /[.]jadec$/).forEach(function(test){
       var actual = fn({ title: 'Jade', interpolated: 'blah blah' }, rt.attrs, rt.escape, rt.rethrow, rt.merge);
       actual.trim().should.equal(html);
     } catch (e) {
-      console.log("\nCoffeeScript:\n" + lineify(coffee));
-      console.log("\nJavaScript:\n" + lineify(js));
+      var ast = jade.parse(str, { filename: test.jade_path, pretty: true, coffee: true });
+      console.log("\nAST:\n" + lineify(ast.pretty(), test.name + '[AST]:'));
+      console.log("\nCoffeeScript:\n" + lineify(coffee, test.name + '[Coffee]:'));
+      console.log("\nJavaScript:\n" + lineify(js, test.name + '[JS]:'));
       throw e
     }
   })
